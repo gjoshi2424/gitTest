c561eb70252f9339b49a1a5adf21d604275c80ab
Kaanan Kharwa <kaanan99@gmail.com>
2021-01-06 14:38:05 -0800

    Added stuff by Kaanan

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,6 @@
-System.out.println("2.5 years at Cal Poly");
+public class HelloWorld{
+   public static void main(String args[]){
+   System.out.println("2.5 years at Cal Poly");
+   System.out.println("Added by Kaanan");
+   }
+}
